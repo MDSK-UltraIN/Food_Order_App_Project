8ec742a88da399620201f93427923037b4791bf2
TW-IT-Jouven Liao <jouven.liao@shoalter.com>
2023-08-11 16:49:53 +0800

    feat: Add Cart Item
    將重覆的商品改為增加數量

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,15 +8,38 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    
-    const updatedItems = state.items.concat(action.item);
+
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+
+    if (existingCartItem) { // 若購物車內已有同樣商品，則增加 amount
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items]; // 複製既有 items 清單
+      updatedItems[existingCartItemIndex] = updatedItem; // 更新已存在之item資料
+
+
+    } else { // 若購物車內沒有已存在商品 
+      updatedItems = state.items.concat(action.item);
+    }
+
 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
-  }else if (action.type === 'REMOVE'){
+  } else if (action.type === 'REMOVE') {
 
   }
 
