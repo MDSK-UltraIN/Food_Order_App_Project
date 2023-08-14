cd44cd179be21e5eaa9a9e780a47273bf733f2bc
TW-IT-Jouven Liao <jouven.liao@shoalter.com>
2023-08-14 10:50:03 +0800

    style: 刪除無使用 Fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Layout/Header";
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
@@ -14,11 +14,11 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   }
-  
+
   return (
     <CartProvider>
-      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
