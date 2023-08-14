ea477fb96127573d107680307d156fbbe11cfbb1
TW-IT-Jouven Liao <jouven.liao@shoalter.com>
2023-08-14 10:42:26 +0800

    feat: Add Removable Cart
    新增減少購物車商品數量功能

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -40,6 +40,26 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   } else if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex]
+    const updatedTotalAmount = state.totalAmount - existingItem.price
+
+    let updateItems;
+
+    if (existingItem.amount === 1) {
+      updateItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updateItem = { ...existingItem, amount: existingItem.amount - 1 }
+      updateItems = [...state.items];
+      updateItems[existingCartItemIndex] = updateItem
+    }
+
+    return {
+      items: updateItems,
+      totalAmount: updatedTotalAmount
+    }
 
   }
 
